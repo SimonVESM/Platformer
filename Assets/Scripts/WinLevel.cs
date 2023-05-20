160900cf4be6a37c4f7297f7d6c63759fd012487
Simon <s.j.p.veninga@pl.hanze.nl>
2023-05-20 16:40:50 +0200

    Added analystics

--- Assets/Scripts/WinLevel.cs
+++ Assets/Scripts/WinLevel.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
+using UnityEngine.Analytics;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Core;
+using Unity.Services.Analytics;
 
 public class WinLevel : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D col)
+    async void Start()
     {
-        Debug.Log("You Win");
+        try
+        {
+            await UnityServices.InitializeAsync();
+            List<string> consentIdentifiers = await AnalyticsService.Instance.CheckForRequiredConsents();
+        }
+        catch(ConsentCheckException e)
+        {
+            //something
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Flag")
+        {
+            Dictionary<string, object> parameters = new Dictionary<string, object>()
+            {
+                {"Fabulous String", "You won" },
+            };
+
+            AnalyticsService.Instance.CustomData("myEvent", parameters);
+
+            Debug.Log("Win");
+        }
     }
 }
